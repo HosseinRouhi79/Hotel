89780fd1740df81cb010e49f4ecc0115636a9eb8
Hossein <hosseinrouhi79@gmail.com>
2023-08-04 20:35:10 +0330

    add room

--- Hotel.go
+++ Hotel.go
@@ -13,19 +13,44 @@ type Room struct {
 var rooms = generateRooms()
 
 func main() {
-	var input int
-	fmt.Println("Enter a number")
-	fmt.Println("1: Room list")
-	fmt.Println("2: Add room")
-	fmt.Println("3: Reserve room")
-	fmt.Scanln(&input)
-	switch input {
-	case 1:
-		for _, value := range rooms {
-			fmt.Printf("ID:%d,  Bed Count:%d,  Type:%s,  Price:%.2f\n", value.ID, value.BedCount, value.Type, value.Price)
+	var input string
+	for input != "exit" {
+		fmt.Println("Enter a number")
+		fmt.Println("1: Room list")
+		fmt.Println("2: Add room")
+		fmt.Println("3: Reserve room")
+		fmt.Scanln(&input)
+		switch input {
+		case "1":
+			getRoomList()
+		case "2":
+			addRoom()
+
 		}
 	}
+}
 
+func getRoomList() {
+	for _, value := range rooms {
+		fmt.Printf("ID:%d,  Bed Count:%d,  Type:%s,  Price:%.2f\n", value.ID, value.BedCount, value.Type, value.Price)
+	}
+}
+
+func getRoomFormInput() Room {
+	room := Room{Status: false}
+	fmt.Println("Enter room information line by line (ID, Type, BedCount, Price)")
+	fmt.Scanln(&room.ID)
+	fmt.Scanln(&room.Type)
+	fmt.Scanln(&room.BedCount)
+	fmt.Scanln(&room.Price)
+
+	return room
+}
+
+func addRoom() {
+	room := getRoomFormInput()
+	rooms = append(rooms, room)
+	fmt.Println("The room added successfully")
 }
 
 func generateRooms() []Room {
